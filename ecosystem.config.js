3d0708991e04f47d3344f86a6f04297585592084
senice222 <sasdasd920@gmail.com>
2024-10-04 21:31:46 +0300

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,17 @@
-module.exports = {
-    apps: [{
-      name: 'app',
-      script: './index.js',
-      watch: true,
-      max_restarts: 9999,
-      restart_delay: 1000, 
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      }
-    }]
-  }
+{
+    "apps": [
+        {
+            "name": "app",
+            "script": "./index.js",
+            "watch": true,
+            "max_restarts": 9999,
+            "restart_delay": 5000,
+            "env": {
+                "NODE_ENV": "development"
+            },
+            "env_production": {
+                "NODE_ENV": "production"
+            }
+        }
+    ]
+}
